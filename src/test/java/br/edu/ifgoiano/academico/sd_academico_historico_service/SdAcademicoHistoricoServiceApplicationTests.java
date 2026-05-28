9e85da335828bc33823f93e6569ccdc5ce70b2dc
Artur Duarte Monteiro <monteiroarturduarte@gmail.com>
2026-05-28 09:47:33 -0300

    Initial commit

---
+++ src/test/java/br/edu/ifgoiano/academico/sd_academico_historico_service/SdAcademicoHistoricoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifgoiano.academico.sd_academico_historico_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdAcademicoHistoricoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
